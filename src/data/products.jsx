e10a72af078e9fec1f65d6be4f8ce71cfa69cffc
EmanuelHSC <emanuelhoffmann0@hotmail.com>
2024-05-08 08:46:52 -0300

    chore(data): update data

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -17,49 +17,49 @@ const productsList = [
     id: 3,
     name: "Sofá de Couro",
     description: "Sofá de couro com design elegante.",
-    image: "https://example.com/sofa-couro.jpg",
+    image: "https://livdecora.com.br/wp-content/uploads/2021/03/Sofa-em-couro-natural-hakira3-scaled.jpg",
     price: "1999,99"
   },
   {
     id: 4,
     name: "Cama Box",
     description: "Cama box com colchão de molas.",
-    image: "https://example.com/cama-box.jpg",
+    image: "https://a-static.mlcdn.com.br/1500x1500/cama-box-casal-queen-size-com-molas-ensacadas-relax-adorabile-marrom-158x198x63cm-ecoflex/colchoesmm/101/151c8620f8b8b97569eea937cea4173f.jpg",
     price: "1499,99"
   },
   { 
     id: 5,
     name: "Guarda-Roupa",
     description: "Guarda-roupa com portas de correr.",
-    image: "https://example.com/guarda-roupa.jpg",
+    image: "https://th.bing.com/th/id/R.03510ef256b94802f903447156ca8da3?rik=twVywjqfx%2fydvQ&pid=ImgRaw&r=0",
     price: "799,99"
   },
   {
     id: 6,
     name: "Geladeira",
     description: "Geladeira frost-free com dispenser de água.",
-    image: "https://example.com/geladeira.jpg",
+    image: "https://th.bing.com/th/id/OIP.EEqJqDhy8FSQTaiJlaN4ewHaHa?rs=1&pid=ImgDetMain",
     price: "2999,99"
   },
   {
     id: 7,
     name: "Fogão",
     description: "Fogão de 5 bocas com forno a gás.",
-    image: "https://example.com/fogao.jpg",
+    image: "https://www.efacil.com.br/wcsstore/ExtendedSitesCatalogAssetStore/Imagens/1000/2217281_01.jpg",
     price: "999,99"
   },
   {
     id: 8,
     name: "Micro-ondas",
     description: "Micro-ondas de 30L com função grill.",
-    image: "https://example.com/microondas.jpg",
+    image: "https://http2.mlstatic.com/microondas-solo-espelhado-preto-lg-30l-220v-D_NQ_NP_973878-MLB28978257001_122018-F.jpg",
     price: "499,99"
   },
   {
     id: 9,
     name: "Máquina de Lavar",
     description: "Máquina de lavar 15kg com função turbo.",
-    image: "https://example.com/maquina-lavar.jpg",
+    image: "https://th.bing.com/th/id/OIP.eYeopFGrmWiVP1U0YJ62VAHaFj?rs=1&pid=ImgDetMain",
     price: "1999,99"
   },
 ];

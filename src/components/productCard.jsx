b1e064678f2d9a78741932e2b0ca207710edf52a
EmanuelHSC <emanuelhoffmann0@hotmail.com>
2024-06-24 23:25:19 -0300

    (productCard)bug: sonarLint

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -9,33 +9,30 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useDispatch } from "react-redux";
 import { Creators as CartActions } from "../store/ducks/cart";
 import Snackbar from "@mui/material/Snackbar";
-import Alert from '@mui/material/Alert';
-
+import Alert from "@mui/material/Alert";
 
 function ProductCard({ name = "Default Name", description = "Default Description", image, price = "0.00", id }) {
   const dispatch = useDispatch();
-  // console.log("Props em ProductCard:", { name, description, image, price });
-  const [openAlert, setOpenAlert] = React.useState(false);
-  const vertical = 'bottom';
-  const horizontal = 'center';
+  const [openAlert, setOpenAlert] = useState(false);
+  const vertical = "bottom";
+  const horizontal = "center";
 
   const handleClickAlert = () => {
     setOpenAlert(true);
   };
 
   const handleCloseAlert = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpenAlert(false);
   };
 
-
   const handleAddToCart = () => {
     console.log("Adicionando ao carrinho:", { name, description, image, price, id });
     dispatch(CartActions.addToCart({ name, description, image, price, id }));
-    handleClickAlert({ vertical: 'bottom', horizontal: 'center' });
+    handleClickAlert();
   };
 
   return (
@@ -48,29 +45,17 @@ function ProductCard({ name = "Default Name", description = "Default Description
         <Typography variant="body2" color="text.secondary">
           {description}
         </Typography>
-          <Typography variant="bo dy1" color="text.primary">
-            R${price}
-          </Typography>
-          <Button variant="contained" sx={{ marginTop: 2 }} onClick={handleAddToCart}>
-            <ShoppingCartIcon color="#ccc"/>
-          </Button>
-          <Snackbar 
-          open={openAlert} 
-          autoHideDuration={6000} 
-          onClose={handleCloseAlert} 
-          anchorOrigin={{ vertical, horizontal}}
-          >
-          <Alert
-            onClose={handleCloseAlert}
-            severity="success"
-            variant="filled"
-            sx={{ width: '100%' }}
-            key={vertical + horizontal}
-
-          >
+        <Typography variant="body1" color="text.primary">
+          R${price}
+        </Typography>
+        <Button variant="contained" sx={{ marginTop: 2 }} onClick={handleAddToCart}>
+          <ShoppingCartIcon />
+        </Button>
+        <Snackbar open={openAlert} autoHideDuration={6000} onClose={handleCloseAlert} anchorOrigin={{ vertical, horizontal }}>
+          <Alert onClose={handleCloseAlert} severity="success" variant="filled" sx={{ width: "100%" }} key={vertical + horizontal}>
             Produto adicionado ao carrinho!
           </Alert>
-      </Snackbar>
+        </Snackbar>
       </CardContent>
     </Card>
   );
